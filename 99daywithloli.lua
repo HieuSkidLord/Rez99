b45a59624b63f65ef9d27d59bcdfb7fd92897f3a
ThoSanLoli <chungtun806@gmail.com>
2025-09-08 19:36:21 +0700

    Add files via upload

--- 99daywithloli.lua
+++ 99daywithloli.lua
@@ -1,7 +1,7 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "Rex Hub [99 night with Loli in the forest]",
+   Name = "Rez Hub [99 night with Loli in the forest]",
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
    LoadingTitle = "99 night in the forest",
    LoadingSubtitle = "by rez",
